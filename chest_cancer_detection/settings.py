1b840be55bce5d682784447f7fcfbed23ffa10ec
modidhruvil42@gmail.com <modidhruvil42@gmail.com>
2023-11-21 17:32:47 -0500

    Added middleware.py

--- chest_cancer_detection/settings.py
+++ chest_cancer_detection/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'detection.middleware.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = "chest_cancer_detection.urls"
